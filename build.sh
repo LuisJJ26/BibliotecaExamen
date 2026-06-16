2561c4cef5d3c44b2edb4b71d94d6bec711b32ea
LuisJJ26 <145565859+LuisJJ26@users.noreply.github.com>
2026-06-16 16:14:30 -0600

    superusuario

--- build.sh
+++ build.sh
@@ -5,3 +5,13 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell -c "
+from usuarios.models import Usuario;
+if not Usuario.objects.filter(username='admin').exists():
+    Usuario.objects.create_superuser(
+        username='admin',
+        email='admin@example.com',
+        password='TuPasswordSegura123'
+    )
+"
